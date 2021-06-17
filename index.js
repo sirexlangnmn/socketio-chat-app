9d5031ed78130ab385a7239a46351eed0cc3646a
Federex A Potolin <36563535+sirexlangnmn@users.noreply.github.com>
2021-06-17 17:03:17 +0800

    update index.js
    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var socket = require("socket.io");
 
 // App setup
 var app = express();
-var server = app.listen(4000, function () {
+
+const port = process.env.PORT || 4000;
+
+var server = app.listen(port, function () {
 	console.log("listening for requests on port 4000,");
 });
 
